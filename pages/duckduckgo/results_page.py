94deb78b6c463f28de5f6ee9c4a4fceaeb299b75
Josh Hall <lostprophet217@gmail.com>
2024-10-03 15:19:52 -0500

    Refactor results pages to use BaseResult class

--- pages/duckduckgo/results_page.py
+++ pages/duckduckgo/results_page.py
@@ -1,15 +1,18 @@
 from selenium.webdriver.common.by import By
 from pages.base.base_page import BasePage
+from pages.base.base_result import BaseResult
 
 
 class DuckDuckGoResultsPage(BasePage):
-    SEARCH_INPUT = (By.NAME, 'q')
 
-    def results_count(self, SEARCH_PHRASE=None) -> int:
-        xpath = f"//*[@id='react-layout']//li//*[contains(text(),'{SEARCH_PHRASE}')]"
+    def __init__(self, browser):
+        super().__init__(browser)
+        self.result = BaseResult(browser)
+
+    def __getattr__(self, name):
+        return getattr(self.result, name)
+
+    def results_count(self, phrase) -> int:
+        xpath = f"//*[@id='react-layout']//li//*[contains(text(),'{phrase}')]"
         elements = self.browser.find_elements(By.XPATH, xpath)
         return len(elements)
-
-    def search_phrase(self) -> str:
-        element = self.browser.find_element(*self.SEARCH_INPUT)
-        return element.get_attribute('value')
